dc1985221fb8084fedbf86d9171d225ee5d93423
gusanare1 <gang_emelec@yahoo.com>
2018-06-19 16:06:38 -0500

    10 cambiado

--- resolucion_1.py
+++ resolucion_1.py
@@ -6,30 +6,28 @@ Mazda = "CX-5, CX-9, BT-50, Sedan, Hatchback"
 import numpy as np
 import random
 marcas_autos = []
-marcas_autos = Toyota.split(",") + Hyunday.split(",") + Nissan.split(",") + Mazda.split(",")
+marcas_autos = Toyota.split(", ") + Hyunday.split(", ") + Nissan.split(", ") + Mazda.split(", ")
 autos_dias = np.zeros( (8,len(marcas_autos)), dtype=int) #Creas matriz
 #print(Toyota+Hyunday+Nissan+Mazda)
 for i in range(0,len(marcas_autos)): #Recorro la matriz
 	for j in range(0, 7):
 		autos_dias[j][i] = random.randrange(1,20)
 	autos_dias[7][i] = random.randrange(1,10)
-print ("1-2-3")
 print(autos_dias)
 print("---------------------------------")
 preg_toyota = autos_dias[:,0:6]
 prom_toyota = np.sum(preg_toyota)/np.sum(autos_dias)*100
 print("4. %4.2f" %(prom_toyota))
 preg_nissan = autos_dias[:,11:16]
-prom_nissan = np.sum(preg_nissan)/np.sum(autos_dias)*100
-print("5. %4.2f" %(prom_nissan))
+print("5. %d" %(np.sum(preg_nissan)))
 preg_hyunday = autos_dias[:, 6:11]
 preg_mazda = autos_dias[:, 16:21]
 print("6. %d Toyota %d Hyunday %d Nissan %d Mazda" % (np.sum(preg_toyota), np.sum(preg_hyunday) ,np.sum(preg_nissan), np.sum(preg_mazda)) )
 intereses = []
-intereses.append(str(np.sum(preg_toyota))+"-Toyota")
-intereses.append(str(np.sum(preg_hyunday))+"-Hyunday")
-intereses.append(str(np.sum(preg_nissan))+"-Nissan")
-intereses.append(str(np.sum(preg_mazda))+"-Mazda")
+intereses.append(str(np.sum(preg_toyota))+"-Toyota+")
+intereses.append(str(np.sum(preg_hyunday))+"-Hyunday+")
+intereses.append(str(np.sum(preg_nissan))+"-Nissan+")
+intereses.append(str(np.sum(preg_mazda))+"-Mazda+")
 print("7. %s" %min(intereses))
 prom_interesados = np.sum(autos_dias)/4
 superaron_promedio_interesados = ""
@@ -45,5 +43,11 @@ print("9. Porc. Real %4.2f Toyota %4.2f Hyunday %4.2f Nissan %4.2f Mazda" %(toyo
 modelos_ventas = {}
 for i in range(0, len(marcas_autos)):
 	modelos_ventas[marcas_autos[i]]=np.sum(autos_dias[:,i])
-print("10. "+str(sorted(modelos_ventas, key=modelos_ventas.get, reverse=True)[:5]))
+print("10.a "+str(sorted(modelos_ventas, key=modelos_ventas.get, reverse=True)[:5]).replace(" ",""))
+#10-2da forma
+modelos_ventas=[]
+for i in range(0,len(marcas_autos)):
+	modelos_ventas.append(str(np.sum(autos_dias[:,i]))+"+"+marcas_autos[i])
+modelos_ventas=sorted(modelos_ventas, reverse=True)
+print("10.b "+str(modelos_ventas[:5]))
 print("11. %d" %(np.sum(autos_dias[:,13]) ))
